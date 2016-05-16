a7a8cf791fa84da27d1153ff36a493c65d036c8c
Thein Htike Aung <thein.thein99@gmail.com>
2016-05-16 20:27:19 +0800

    First Commit

---
+++ Classes/ZFModalTransitionAnimator.h
@@ -0,0 +1,38 @@
+//
+//  ZFModalTransitionAnimator.h
+//
+//  Created by Amornchai Kanokpullwad on 5/10/14.
+//  Copyright (c) 2014 zoonref. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <UIKit/UIGestureRecognizerSubclass.h>
+
+typedef NS_ENUM(NSUInteger, ZFModalTransitonDirection) {
+    ZFModalTransitonDirectionTop, 
+    ZFModalTransitonDirectionBottom,
+    ZFModalTransitonDirectionLeft,
+    ZFModalTransitonDirectionRight,
+};
+
+@interface ZFDetectScrollViewEndGestureRecognizer : UIPanGestureRecognizer
+@property (nonatomic, weak) UIScrollView *scrollview;
+@property (nonatomic) ZFModalTransitonDirection direction;
+@end
+
+@interface ZFModalTransitionAnimator : UIPercentDrivenInteractiveTransition <UIViewControllerAnimatedTransitioning, UIViewControllerTransitioningDelegate, UIGestureRecognizerDelegate>
+
+@property (nonatomic, assign, getter=isDragable) BOOL dragable;
+@property (nonatomic, readonly) ZFDetectScrollViewEndGestureRecognizer *gesture;
+@property (nonatomic, assign) UIGestureRecognizer *gestureRecognizerToFailPan;
+@property BOOL bounces;
+@property (nonatomic) ZFModalTransitonDirection direction;
+@property CGFloat behindViewScale;
+@property CGFloat behindViewAlpha;
+@property CGFloat transitionDuration;
+
+- (id)initWithModalViewController:(UIViewController *)modalViewController;
+- (void)setContentScrollView:(UIScrollView *)scrollView;
+
+@end
